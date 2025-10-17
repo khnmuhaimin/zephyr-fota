d6dd84abae93833fe4e4165378ab6ee16656940c
Muhaimin Khan <khnmuhaimin@gmail.com>
2025-10-17 11:05:06 +0200

    Renamed app-cellular-update to app-cellular-hello

--- run.sh
+++ run.sh
@@ -5,14 +5,14 @@
 
 COMMAND=$1
 
-if [ "$COMMAND" == "build-cellular-update" ]; then
-    echo "Building app-cellular-update..."
-	rm -rf apps/build-cellular-update
-    west build -b esp32_devkitc/esp32/procpu --pristine -d apps/build-cellular-update apps/app-cellular-update/ -- -DDTC_OVERLAY_FILE="boards/esp32-overlay.dts"
+if [ "$COMMAND" == "build-cellular-hello" ]; then
+    echo "Building app-cellular-hello..."
+	rm -rf apps/build-cellular-hello
+    west build -b esp32_devkitc/esp32/procpu --pristine -d apps/build-cellular-hello apps/app-cellular-hello/ -- -DDTC_OVERLAY_FILE="boards/esp32-overlay.dts"
 
-elif [ "$COMMAND" == "build-cellular-update-menuconfig" ]; then
-	echo "Building menuconfig for app-cellular-update..."
-	west build -b esp32_devkitc/esp32/procpu -d apps/build-cellular-update -t menuconfig apps/app-cellular-update
+elif [ "$COMMAND" == "build-cellular-hello-menuconfig" ]; then
+	echo "Building menuconfig for app-cellular-hello..."
+	west build -b esp32_devkitc/esp32/procpu -d apps/build-cellular-hello -t menuconfig apps/app-cellular-hello
 
 elif [ "$COMMAND" == "build-hello-world" ]; then
     echo "Building app-hello-world..."
